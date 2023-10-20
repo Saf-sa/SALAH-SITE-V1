18efd7a43218864c7dc94e6d290ed0f51db6e0bd
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-10-20 16:02:22 +0200

    updated nav.js

---
+++ Assets/nav-footer/nav.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Initialise le comportement du dropdown
+    $('.dropdown-toggle').dropdown();
+  });
+  
+  // Add slideDown animation to dropdown
+  $('.dropdown').on('show.bs.dropdown', function(e){
+      $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+    });
+    
+    // Add slideUp animation to dropdown
+    $('.dropdown').on('hide.bs.dropdown', function(e){
+      $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400);
+    });
