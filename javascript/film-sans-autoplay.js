df1f456ce690326be0330ec1fc1e316e28fa90f0
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-10-22 13:38:27 +0200

    updated film-sans-atoplay.js

---
+++ javascript/film-sans-autoplay.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const videoPlayer = document.querySelector(".video video");
+    const playPauseButton = document.querySelector(".play-pause");
+    const leftButton = document.querySelector(".left");
+    const rightButton = document.querySelector(".right");
+  
+    const videoSources = [
+      "./Assets/Video/film/muse.mp4",
+      "./Assets/Video/film/KomUnik.mp4",
+      "./Assets/Video/film/let's groove.mp4",
+    ];
+    let currentVideoIndex = 0; // Index de la vidéo en cours
+  
+    // Fonction pour mettre en pause ou jouer la vidéo
+    function togglePlayPause() {
+      if (videoPlayer.paused) {
+        videoPlayer.play();
+        playPauseButton.setAttribute("icon", "fluent:video-pause-24-regular");
+      } else {
+        videoPlayer.pause();
+        playPauseButton.setAttribute("icon", "fluent:video-play-24-regular");
+      }
+    }
+  
+    // Fonction pour charger la vidéo suivante
+    function loadNextVideo() {
+      currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
+      videoPlayer.src = videoSources[currentVideoIndex];
+      videoPlayer.pause();
+      playPauseButton.setAttribute("icon", "fluent:video-play-24-regular");
+    }
+  
+    // Fonction pour charger la vidéo précédente
+    function loadPrevVideo() {
+      currentVideoIndex =
+        (currentVideoIndex - 1 + videoSources.length) % videoSources.length;
+      videoPlayer.src = videoSources[currentVideoIndex];
+      videoPlayer.pause();
+      playPauseButton.setAttribute("icon", "fluent:video-play-24-regular");
+    }
+  
+    // Attribution des fonctions aux boutons
+    playPauseButton.onclick = togglePlayPause;
+    leftButton.onclick = loadPrevVideo;
+    rightButton.onclick = loadNextVideo;
+    
+    // Chargement de la première vidéo
+    videoPlayer.src = videoSources[currentVideoIndex];
+  });
